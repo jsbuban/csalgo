22d896aed558cd495665e343d36daef8e86bd4c0
Jayson Alvin Buban <jbuban29@gmail.com>
2016-10-30 16:48:42 +0800

    Add files via upload

---
+++ GCD.java
@@ -0,0 +1,27 @@
+
+package gcd;
+
+import java.util.*;
+
+public class GCD {
+    public static void main(String args[])throws Exception {
+        
+            Scanner sc = new Scanner(System.in);
+                System.out.print("Enter the First no : ");
+            int n1=sc.nextInt();
+                System.out.print("Enter the Second no : ");
+            int n2=sc.nextInt();
+             
+            while(n1 != n2)
+            {
+                if(n1 > n2)
+                    n1 = n1-n2;
+                else
+                    n2 = n2-n1;
+            }
+                System.out.println("GCD = "+n1);
+    }
+}
+    
+    
+
